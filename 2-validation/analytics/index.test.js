b0b7635ed99e36557eacd76364eeb5e8a880e339
tarasowski <dimitri+github@tarasowski.de>
2019-05-31 16:55:32 +0200

    schemas finished / covered w/ tests

--- 2-validation/analytics/index.test.js
+++ 2-validation/analytics/index.test.js
@@ -1,12 +1,42 @@
 const {main} = require("./index.js")
-const payload = require("./payload.json")
+const payloadIdentity = require("./payload_identity.json")
+const payloadPage = require("./payload_page.json")
+const payloadAction = require("./payload_action.json")
+const payloadTransaction = require("./payload_transaction.json")
 const {describe} = require("riteway")
 
 describe("main()", async assert =>
   assert({
-    given: "json payload",
-    should: "validate fields",
-    actual: await main(payload).catch(e => console.log(e) || e),
-    expected: "success"
+    given: "json pipes.identity() payload",
+    should: "validate required and not-required fields",
+    actual: await main(payloadIdentity).catch(e => console.log(e) || e),
+    expected: "valid"
+  })
+)
+
+describe("main()", async assert =>
+  assert({
+    given: "json pipes.page() payload",
+    should: "validate required and not-required fields",
+    actual: await main(payloadPage).catch(e => console.log(e) || e),
+    expected: "valid"
+  })
+)
+
+describe("main()", async assert =>
+  assert({
+    given: "json pipes.action() payload",
+    should: "validate required and not-required fields",
+    actual: await main(payloadAction).catch(e => console.log(e) || e),
+    expected: "valid"
+  })
+)
+
+describe("main()", async assert =>
+  assert({
+    given: "json pipes.transaction() payload",
+    should: "validate required and not-required fields",
+    actual: await main(payloadTransaction).catch(e => console.log(e) || e),
+    expected: "valid"
   })
 )
