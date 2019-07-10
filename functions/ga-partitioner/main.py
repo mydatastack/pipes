f129ba7baf7e3a6b33f56eb9bed6962ee3f6c504
tarasowski <dimitri+github@tarasowski.de>
2019-07-10 14:09:40 +0200

    working version saving to s3

--- functions/ga-partitioner/main.py
+++ functions/ga-partitioner/main.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 from urllib.parse import unquote
 from itertools import groupby
 import boto3
+import time
 
 s3 = boto3.resource('s3')
 pipe = lambda *args: lambda x: reduce(lambda a, fn: fn(a), args, x)
@@ -32,6 +33,7 @@ def load_file(m: list) -> str:
         return obj.get()['Body'].read().decode('utf-8')
     except Exception as e:
         print(e) 
+        return e
 
 
 def split_str(data: str) -> list: 
@@ -52,21 +54,22 @@ def decode_json(data: list) -> list:
 
 def take_props(data: list) -> list:
     return [
-            (el['body']['ds'], el['body']['t'], el) 
+            (el['body']['tid'].lower(), el['body']['ds'], el['body']['t'], el) 
             for el in data
            ]
 
 def group_by_ds(data: list) -> list:
     return [
-            (ds, event, list(dt for ds, ev, dt in data)) 
-            for ds, g1 in groupby(data, key=lambda x: x[0])
-            for event, data in groupby(g1, key=lambda x: x[1])
+            (tid, ds, event, list(dt for tid, ds, ev, dt in data)) 
+            for tid, g1 in groupby(data, key=lambda x: x[0])
+            for ds, g2 in groupby(g1, key=lambda x: x[1])
+            for event, data in groupby(g2, key=lambda x: x[2])
             ]
 
 def sort_data(data: list) -> list:
-    return sorted(data, key=lambda t: (t[0],t[1])) 
+    return sorted(data, key=lambda t: (t[0],t[1],t[2])) 
 
-def construct_keys(event: dict, data: list):
+def construct_keys(event: dict, data: list) -> list:
      keys = pipe(
                 get_records,
                 get_list,
@@ -75,12 +78,23 @@ def construct_keys(event: dict, data: list):
      bucket = keys[0].bucket
      folders = keys[0].key.split('/')
      base_folders = "/".join(folders[:2])
-     partition_folders = "/".join(folders[2:5])
+     partition_folders = "/".join(folders[2:6])
      return [
-             (bucket, base_folders + '/' + ds + '/' + event + '/' + partition_folders, ds, event, body) 
-             for ds, event, body in data
+             (bucket, tid, base_folders + '/' + tid + '/' + ds + '/' + event + '/' + partition_folders, ds, event, body) 
+             for tid, ds, event, body in data
             ]
-   
+
+def save_to_s3(data: list, ts=time.strftime("%Y-%m-%dT%H:%M:%S%z", time.gmtime())) -> str:
+    try:
+        for bucket, tid, folder, ds, event, body in data:
+            key = f'{tid}-{event}-{ts}'
+            body_json = [json.dumps(record) for record in body]
+            new_line_delimited = '\n'.join(body_json)
+            s3.Object(bucket, folder + '/' + key).put(Body=new_line_delimited)
+        return 'success'
+    except Exception as e:
+        print(e) 
+        return e
 
 def handler(event, ctx):
     return pipe( 
@@ -95,7 +109,6 @@ def handler(event, ctx):
                 sort_data,
                 group_by_ds,
                 partial(construct_keys, event),
-               (lambda x: print(json.dumps(x, indent=4, separators=(',',':')))),
-                #merge_tuples,
-                #(lambda x: print(x))
+                save_to_s3,
+               (lambda x: print(json.dumps(x, indent=4, separators=(',',':'))) or x),
                )(event)
